60eeb1493daf53af8570a4537fb151f4e3e1910d
Javiera Tapia <javieratapiastahl@gmail.com>
2020-01-25 16:21:09 -0300

    Portafolio Completo

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ $(function () {
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+$(function(){
+  $("a").click(function(event) {
+  if (this.hash !== "") {
+  event.preventDefault();
+  var gato = this.hash;
+  $('html, body').animate({
+  scrollTop: $(gato).offset().top
+  },
+   800, function(){
+  window.location.hash = gato;
+  });
+  }
+  });
+
+});
